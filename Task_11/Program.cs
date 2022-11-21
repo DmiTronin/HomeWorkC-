f6c9f4c9b03cf872dd86baa7b936e3e210f9d981
Dmitiy <Dmitronin87@gmail.com>
2022-11-21 13:50:52 +0300

    Task_11

---
+++ Task_11/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая выводит трехзначное число и удаляет вторую цифру этого числа.
+int randomNumber = new Random().Next(100,1000);
+int lastDigit = randomNumber % 10;
+int firstDigit = randomNumber / 100;
+
+Console.WriteLine(randomNumber);
+Console.WriteLine($"{firstDigit}{lastDigit}");
