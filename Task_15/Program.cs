7315f30b1d4b6693fc4fbe07008d553df1a10f6f
Dmitiy <Dmitronin87@gmail.com>
2022-11-23 07:52:54 +0300

    Task_15v.1.1

--- Task_15/Program.cs
+++ Task_15/Program.cs
@@ -7,11 +7,11 @@
 
 Console.Write("Введите число от 1 до 7 обозначющее день недели: ");
 int n = Convert.ToInt32(Console.ReadLine());
-if (n <= 5)
+if (n == 5 ||n == 1 || n == 4 || n == 3 || n == 2)
 {
 Console.WriteLine("Будний день");
 }
-else
+else if( n == 6 || n == 7)
 {
 Console.WriteLine("Выходной день");
 }
